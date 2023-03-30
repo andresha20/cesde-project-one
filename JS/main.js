79ad6f24e028dc4bb93b205790f4d97c5ec7ed06
andresha20 <andreshenao.2021@gmail.com>
2023-03-30 18:34:54 -0500

    30/03/2023 - 18:34

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const url_arr = document.URL.split('/');
+const route = url_arr[url_arr.length - 1];
+const routeName = route.split('.');
+
+document.getElementById(routeName[0]).classList.add('active-menu-item');
